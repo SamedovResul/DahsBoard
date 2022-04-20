817b0fac8c85bb92b8e474c6b826d04275da8f20
SamedovRasul <samedovrasul7@gmail.com>
2022-04-20 14:09:12 +0400

    solve problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 
-function App() {
-  return (
-   <>
-   <h1>its work</h1>
-   </>
-  );
+const App = () =>{
+  return(
+    <>
+    <h1>its working</h1>
+    </>
+  )
 }
 
 export default App;
