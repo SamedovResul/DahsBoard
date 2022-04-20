9f069e1b11023d560fd6ea95595e405a837ddc0b
SamedovRasul <samedovrasul7@gmail.com>
2022-04-20 14:15:29 +0400

    solve problem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { io } from "socket.io-client";
 import {useSpring, animated} from 'react-spring';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import IndividualAvarage from './component/dataChartTemplates/IndividualAvarage';
+import GeneralAvarage from './component/dataChartTemplates/GeneralAvarage'
+import SideBar from './component/sideBar/SideBar';
+import TopBar from './component/topBar/TopBar'
+import Excelentboard from './component/Emplyeefolder/Excelentboard';
+import Map from './component/maps/Map';
+import News from './component/news/News';
+import GeneralChart from './component/charts/GeneralChart'
+import Navbar from './component/nav-bar/NavBar'
+import Data from './data/data';
 
 const App = () =>{
   return(
